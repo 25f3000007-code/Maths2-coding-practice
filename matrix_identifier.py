65d2aec0eaf8d67cdec9f0fc8c6534ffe22bdd53
Aditya Roy <25f3000007@ds.study.iitm.ac.in>
2026-05-29 09:01:52 +0530

    mistake log

--- matrix_identifier.py
+++ matrix_identifier.py
@@ -11,10 +11,20 @@ for i in range(1, rows+1):
     r = (input(f'Enter row{i}: ').split())
     matrix.append(r)
     
-        
-print(matrix)
+print(matrix)     
+def get_diagonal():
+    diagonal = []
+    for i in range(0, rows+1):
+        temp_list = matrix[i]
+        element = temp_list[i]
+        diagonal.append(element)
+    return(diagonal)
 
+print(get_diagonal())
 
+'''You loop with for i in range(1, rows+1):
+Python lists are zero-indexed, so valid row indices are 0 to rows-1
+When i == rows, matrix[i] is out of range and raises IndexError'''
 
     
 
