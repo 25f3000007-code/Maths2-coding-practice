a793472f3e55d4cef600554feb78ab198f3087fd
Aditya Roy <25f3000007@ds.study.iitm.ac.in>
2026-06-03 20:14:03 +0530

    added upper triangular matrix feature in matrix identified.py

--- matrix_identifier.py
+++ matrix_identifier.py
@@ -28,17 +28,32 @@ x = diagonal_and_non_diognal_elements()
 argument1  = x[0]
 argument2 = x[1]
 
-def matrix_check(diagonal, non_diagonal):
+
+               
+
+
+def upper_triangular(matrix):
+    lower_element = []
+    for i in range(1, rows):
+        for j in range(0, i):
+            lower_element.append(matrix[i][j])
+    return(lower_element)
+
+arguement3 = upper_triangular(matrix)
+                        
+def matrix_check(diagonal, non_diagonal, lower_element):
     square_matrix = rows == columns
     diagonal_matrix = False
     scalar_matrix = False
+    upper_triangular = False
 
     if square_matrix and all(item == "0" for item in non_diagonal):
         diagonal_matrix = True
         target = diagonal[0]
         scalar_matrix = all(item == target for item in diagonal)
+    if square_matrix:
+        upper_triangular = all(item == '0' for item in lower_element)
 
-    return(f'Square Matrix: {square_matrix}\nDiagonal Matrix: {diagonal_matrix}\nScalar Matrix: {scalar_matrix}')
-    
-               
-print(matrix_check(argument1, argument2))
+    return(f'Square Matrix: {square_matrix}\nDiagonal Matrix: {diagonal_matrix}\nScalar Matrix: {scalar_matrix}\nUpper Triangular Matrix: {upper_triangular}')
+
+print(matrix_check(argument1, argument2, arguement3))
